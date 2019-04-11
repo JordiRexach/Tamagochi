d6a8f4017700e23813e4e01c292ae8ffe53b56a8
JordiRexach <jordi.rexach13@gmail.com>
2019-04-11 00:15:44 -0300

    Terminados test de cuando la mascota come (faltan los de cuando juega)

--- src/modelo/Tamagochi.java
+++ src/modelo/Tamagochi.java
@@ -3,11 +3,32 @@ package modelo;
 public class Tamagochi {
 	private Boolean estaHambrienta = true;
 	private int nivelFelicidad = 0;
+	private int tiempoDeMalHumor = 0;
+	
+	public void setEstaHambrienta(Boolean estaHambrienta) {
+		this.estaHambrienta = estaHambrienta;
+	}
+	
+	public int getNivelFelicidad() {
+		return nivelFelicidad;
+	}
+	
+	public int getTiempoDeMalHumor() {
+		return tiempoDeMalHumor;
+	}
+
+	public void setTiempoDeMalHumor(int tiempoDeMalHumor) {
+		this.tiempoDeMalHumor = tiempoDeMalHumor;
+	}
 
 	public void comer() {
 		if (estaHambrienta){
 			nivelFelicidad += 1;
-		}		
+		}
+		
+		else if (!estaContenta() & tiempoDeMalHumor > 80) {
+			nivelFelicidad = 1;
+		}
 	}
 
 	public boolean estaContenta() {		
--- test/modelo/TamagochiTest.java
+++ test/modelo/TamagochiTest.java
@@ -12,4 +12,30 @@ public class TamagochiTest {
 		mascota.comer();
 		Assert.assertTrue("La mascota no esta contenta", mascota.estaContenta());
 	}
+	
+	@Test
+	public void comer_LaMascotaEstaFelizYSubeUnNivel(){
+		Tamagochi mascota = new Tamagochi();
+		int nivelAnterior = mascota.getNivelFelicidad();
+		mascota.comer();
+		Assert.assertTrue("La mascota no sube un nivel de felicidad", mascota.getNivelFelicidad() > nivelAnterior);
+	}
+	
+	@Test
+	public void comer_LaMascotaEstaDeMalHumorYSePoneFeliz() {
+		Tamagochi mascota = new Tamagochi();
+		mascota.setTiempoDeMalHumor(85);
+		mascota.setEstaHambrienta(false);
+		mascota.comer();
+		Assert.assertTrue("La mascota no se pone feliz", mascota.estaContenta());
+	}
+	
+	@Test
+	public void comer_LaMascotaEstaDeMalHumorYNoSePoneFeliz() {
+		Tamagochi mascota = new Tamagochi();
+		mascota.setTiempoDeMalHumor(40);
+		mascota.setEstaHambrienta(false);
+		mascota.comer();
+		Assert.assertTrue("La mascota se pone feliz", !mascota.estaContenta());
+	}
 }
