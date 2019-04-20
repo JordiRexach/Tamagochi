bb8d0434411809f0e1aa8e19156c12d6637262a7
usueldo <usueldo@gmail.com>
2019-04-19 23:21:07 -0300

    tests

--- src/modelo/Tamagochi.java
+++ src/modelo/Tamagochi.java
@@ -1,58 +1,71 @@
 package modelo;
 
+import java.util.stream.IntStream;
+
 public class Tamagochi {
-	private Boolean estaHambrienta = true;
-	private int nivelFelicidad = 0;
-	private int tiempoDeMalHumor = 0;
+	private static int limiteDetiempoMalHumor = 80;
+	private Estado estado = new Estado();
+	private int cantidadDeVecesJugados = 0; //No jugo nunca 
 	
-	public void setEstaHambrienta(Boolean estaHambrienta) {
-		this.estaHambrienta = estaHambrienta;
-	}
-	
-	public int getNivelFelicidad() {
-		return nivelFelicidad;
-	}
-	
-	public void setNivelFelicidad(int nivelFelicidad) {
-		this.nivelFelicidad = nivelFelicidad;
-	}
-	
-	public int getTiempoDeMalHumor() {
-		return tiempoDeMalHumor;
+	public boolean estaContenta() {
+		return estado.esContenta();
 	}
 
-	public void setTiempoDeMalHumor(int tiempoDeMalHumor) {
-		this.tiempoDeMalHumor = tiempoDeMalHumor;
+	public boolean estaHambrienta() {
+		return estado.esHambrienta();
 	}
 
+	public boolean estaDeMalHumor() {
+		return estado.esMalHumor();
+	}
+	
 	public void comer() {
-		if (estaHambrienta){
-			nivelFelicidad += 1;
-		}
-		
-		else if (!estaContenta() & tiempoDeMalHumor > 80) {
-			nivelFelicidad = 1;
+		if(estado.esHambrienta()) {
+			estado.cambiarAContenta();
+			estado.subirNivelFelicidad();
+		}else if(estado.esMalHumor() && estado.getTiempoMalHumor() > limiteDetiempoMalHumor){
+			estado.cambiarAContenta();
 		}
 	}
 
-	public boolean estaContenta() {		
-		return nivelFelicidad > 0;
+	public void jugar() {
+		//Se incrementa el nivel de felicidad dos veces
+		IntStream.rangeClosed(1, 2).forEach(i -> estado.subirNivelFelicidad());
+		//Se incrementa las veces que jugo la mascota
+		this.incrementarCantidadDeVecesJugado();
+		//Se comprueba si estaba de mal humor
+		
+		if(estado.esMalHumor()) {
+			//Se pone contenta
+			estado.cambiarAContenta();
+		}else if(estado.esHambrienta()) {
+			estado.cambiarAMalHumor(1);
+		}
+		
+		if( this.jugoMasDe5Veces()) {
+			estado.cambiarAHambrienta();
+		}
+		
+		//System.out.println(cantidadDeVecesJugados);
 	}
 	
-	public void jugar() {
-		if (!estaHambrienta) {
-			if(estaContenta()) {
-				nivelFelicidad += 2;
-			}
-			
-			else {
-				nivelFelicidad = 1;
-			}
-		}
-		
-		else {
-			nivelFelicidad = 0;
-		}
+	private void incrementarCantidadDeVecesJugado() {
+		cantidadDeVecesJugados += 1;
 	}
 
+	private boolean jugoMasDe5Veces() {
+		return cantidadDeVecesJugados > 5;
+	}
+
+	public int getNivel() {
+		return estado.getNivelFelicidad();
+	}
+	
+	public void sePoneDeMalHumor(int tiempoMalHumor) {
+		estado.cambiarAMalHumor(tiempoMalHumor);
+	}
+
+	public void sePoneContenta() {
+		estado.cambiarAContenta();
+	}
 }
